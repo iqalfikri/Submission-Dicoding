5e8c3216e96e2a676e08d6f71d2b169e710c775e
iqaltech <iqalfikri@gmail.com>
2021-01-08 06:44:07 +0700

    Inisialisasi Script

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+console.log("JavaScript berhasil digunakan");
+
+//edited profile
+document.getElementById('name').innerHTML = 'Muhammad Fikri Haikal';
+document.getElementById('date').innerHTML = '21 Oktober 2002';
+document.getElementById('age').innerHTML = '17 Tahun';
+document.getElementById('city').innerHTML = 'Kota Medan';
+document.getElementById('religion').innerHTML = 'Islam';
+document.getElementById('graduate').innerHTML = 'SMK Tritech Informatika';
